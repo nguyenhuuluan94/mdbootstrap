1d58c14038bb3d9ba0043515ac8e319cd6e5dda4
Nguyen Huu Luan <luan.nguyen@swissitbridge.local>
2017-12-13 14:49:45 +0700

    add responsive

--- src/app/homepage/course/course.component.ts
+++ src/app/homepage/course/course.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
 
+  imgs = [
+    {
+      url: 'https://undsgn-firmadamianobedu.netdna-ssl.com/uncode/wp-content/uploads/2017/04/demo-content-homepage-Classic-Agency-Uncode-uai-480x320.jpg'
+    },
+    {
+      url: 'https://undsgn-firmadamianobedu.netdna-ssl.com/uncode/wp-content/uploads/2017/05/demo-content-homepage-Creative-Digital-Agency-Uncode-uai-480x320.jpg'
+    },
+    {
+      url: 'https://undsgn-firmadamianobedu.netdna-ssl.com/uncode/wp-content/uploads/2017/05/demo-content-homepage-Creative-Corporation-Uncode-uai-480x320.jpg'
+    },
+    {
+      url: 'https://undsgn-firmadamianobedu.netdna-ssl.com/uncode/wp-content/uploads/2017/04/demo-content-homepage-Blog-Magazine-Uncode-uai-480x320.jpg'
+    },
+    {
+      url: 'https://undsgn-firmadamianobedu.netdna-ssl.com/uncode/wp-content/uploads/2017/04/demo-content-homepage-Blog-Metro-Uncode-uai-480x320.jpg'
+    },
+    {
+      url: 'https://undsgn-firmadamianobedu.netdna-ssl.com/uncode/wp-content/uploads/2017/04/demo-content-homepage-Blog-Editorial-Uncode-uai-480x320.jpg'
+    },
+    {
+      url: 'https://undsgn-firmadamianobedu.netdna-ssl.com/uncode/wp-content/uploads/2017/04/demo-content-homepage-Classic-Start-Up-Uncode-uai-480x320.jpg'
+    },
+    {
+      url: 'https://undsgn-firmadamianobedu.netdna-ssl.com/uncode/wp-content/uploads/2017/04/demo-content-homepage-Creative-Agency-Uncode-uai-480x320.jpg'
+    },
+    {
+      url: 'https://undsgn-firmadamianobedu.netdna-ssl.com/uncode/wp-content/uploads/2017/04/demo-content-homepage-Creative-Agency-Uncode-uai-480x320.jpg'
+    },
+    {
+      url: 'https://undsgn-firmadamianobedu.netdna-ssl.com/uncode/wp-content/uploads/2017/04/demo-content-homepage-Portfolio-Metro-Uncode-uai-480x320.jpg'
+    },
+    {
+      url: 'https://undsgn-firmadamianobedu.netdna-ssl.com/uncode/wp-content/uploads/2015/10/demo-content-homepage-Creative-Department-Uncode-uai-480x320.jpg'
+    },
+    {
+      url: 'https://undsgn-firmadamianobedu.netdna-ssl.com/uncode/wp-content/uploads/2017/04/demo-content-homepage-Classic-Restaurant-Uncode-uai-480x320.jpg'
+    },
+    
+  ]
   constructor() { }
 
   ngOnInit() {
